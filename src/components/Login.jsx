129f71148a7443cf0eb36bdaaee7d39953e9599c
Sadgeness <sadgenesss@gmail.com>
2025-01-18 16:58:59 +0800

    trust me bro, i got this 8)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-
-  useEffect(() => {
-    // Prevent scrolling when the component mounts
-    document.body.style.overflow = 'hidden';
-
-    // Clean up when the component unmounts
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,20 +23,20 @@ function Login() {
 
   return (
     <>
-      {/* Set background image and make the page unscrollable */}
+      {/* Set background image and make the page scrollable */}
       <div
         className="min-h-screen bg-cover bg-center"
         style={{ backgroundImage: 'url(https://i.imgur.com/d6dRrzK.jpeg)' }}
       >
-        <header className="bg-black text-white py-8 px-12 flex justify-between items-center shadow-xl">
-          <div className="text-5xl font-bold">
-            <span className="text-yellow-400">Aral</span>
-            <span className="text-white">Kademy</span>
+        <header className="bg-[#121212] text-[#F6BA18] py-[1.5vw] px-[2vw] flex justify-between items-center shadow-xl">
+          <div className="text-[1.5vw] font-bold">
+            <span className="text-[#F6BA18]">Aral</span>
+            <span className="text-[#FFFFFF]">Kademy</span>
           </div>
 
           <button
             onClick={handleEnroll}
-            className="bg-yellow-400 text-black font-semibold py-4 px-10 rounded-md hover:bg-yellow-500 transition-colors text-xl"
+            className="bg-[#F6BA18] text-[#212529] font-bold py-[0.5vw] px-[2vw] rounded-md transition-colors text-[1vw]"
           >
             Enroll
           </button>
@@ -54,53 +44,54 @@ function Login() {
 
         {/* Main Content */}
         <div className="flex items-center justify-center min-h-screen">
-          <div className="flex items-center p-20 rounded-lg space-x-[400px]" style={{ marginTop: '-100px' }}>
+          <div
+            className="flex flex-col md:flex-row items-center p-[2vw] rounded-lg gap-[5vw]"
+          >
 
             {/* Left Side: Title and Subtitle */}
-            <div className="text-left" style={{ marginTop: '-200px' }}>
-              <h1 className="text-8xl font-extrabold drop-shadow-[5px_5px_5px_rgba(0,0,0,0.8)]">
-                <span className="text-yellow-400">Aral</span>
-                <span className="text-white">Kademy</span>
+            <div className="text-left md:block hidden" style={{ marginTop: '-100px' }}>
+              <h1 className="text-[4vw] font-extrabold drop-shadow-[5px_5px_5px_rgba(0,0,0,0.8)]">
+                <span className="text-[#F6BA18]">Aral</span>
+                <span className="text-[#FFFFFF]">Kademy</span>
               </h1>
-              <p className="text-white mt-4 max-w-md drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] text-2xl">
+              <p className="text-[#FFFFFF] mt-[1vw] max-w-md drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] text-[1.2vw]">
                 Learning Management System for UST NSTP-LTS and Partner Communities
               </p>
             </div>
 
             {/* Right Side: Login Form */}
-            <div className="bg-white p-12 rounded-lg shadow-2xl w-[600px] relative">
-
+            <div className="bg-white p-[3vw] rounded-lg shadow-2xl w-[33vw] relative">
               {/* Thick Yellow Top Border */}
-              <div className="absolute top-0 left-0 w-full h-4 bg-yellow-400 rounded-t-lg"></div>
+              <div className="absolute top-0 left-0 w-full h-[0.5vw] bg-[#F6BA18] rounded-t-lg"></div>
 
-              <h2 className="text-4xl font-bold mb-6 text-left">Login</h2>
-              <p className="text-xl text-gray-500 text-left mb-10">
+              <h2 className="text-[2.5vw] font-bold mb-[1vw] text-left text-[#212529]">Log In</h2>
+              <p className="text-[1vw] text-[#64748B] text-left mb-[1.5vw]">
                 Please fill in your login information to proceed
               </p>
 
-              <form onSubmit={handleSubmit} className="space-y-8">
+              <form onSubmit={handleSubmit} className="space-y-[1vw]">
                 <div>
-                  <label htmlFor="email" className="block text-xl font-medium text-gray-700">Email</label>
+                  <label htmlFor="email" className="block text-[1vw] text-[#64748B]">Email</label>
                   <input
                     type="email"
                     id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
-                    className="mt-3 w-full px-8 py-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 text-xl"
+                    className="mt-[0.5vw] w-full px-[2vw] py-[1vw] border border-[#64748B] rounded-md focus:outline-none focus:ring-2 focus:ring-[#64748B] text-[#64748B]"
                     placeholder="Enter your email"
                   />
                 </div>
 
                 <div>
-                  <label htmlFor="password" className="block text-xl font-medium text-gray-700">Password</label>
+                  <label htmlFor="password" className="block text-[1vw] text-[#64748B]">Password</label>
                   <input
                     type="password"
                     id="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
-                    className="mt-3 w-full px-8 py-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 text-xl"
+                    className="mt-[0.5vw] w-full px-[2vw] py-[1vw] border border-[#64748B] rounded-md focus:outline-none focus:ring-2 focus:ring-[#64748B] text-[#64748B]"
                     placeholder="Enter your password"
                   />
                 </div>
@@ -110,7 +101,7 @@ function Login() {
                   <button
                     type="button"
                     onClick={handleForgotPassword}
-                    className="text-xl text-gray-500 hover:underline focus:outline-none"
+                    className="text-[1vw] text-[#64748B] hover:underline focus:outline-none"
                   >
                     Forgot Password?
                   </button>
@@ -120,7 +111,7 @@ function Login() {
                 <div className="flex justify-center">
                   <button
                     type="submit"
-                    className="bg-black text-white py-4 px-12 rounded-md hover:bg-gray-800 transition-colors text-xl"
+                    className="bg-[#212529] text-[#FFFFFF] py-[0.6vw] px-[4vw] font-semibold rounded-md"
                   >
                     Login
                   </button>
