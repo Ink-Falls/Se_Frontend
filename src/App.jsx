e161ef9f8907b9daa4b36227a9b0379c47220b48
stiipp <curilansteph456@gmail.com>
2025-01-21 14:40:52 +0800

    add basic dashboard, add new components (Sidebar, UserStats, UserTable), add Poppins font

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar, { SidebarItem } from "./components/Sidebar.jsx";
-import Announcement from "./components/Announcement.jsx";
+import Announcement from "./components/StudentDashboard.jsx";
 import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx";
 import "./icon.css";
+import StudentDashboard from "./components/StudentDashboard.jsx";
 
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Announcements" element={<Announcement />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<StudentDashboard />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/StudentDashboard" element={<StudentDashboard />} />
+      </Routes>
     </Router>
   );
 }
