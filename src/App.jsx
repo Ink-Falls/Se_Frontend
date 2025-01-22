55b4ec0b5e894dae782f2c8a4da18e03bbbcbfcf
Ink Falls <aceualcii@gmail.com>
2025-01-22 22:20:56 +0800

    add Header and StudentDashboard components, implement UserStats and UserTable, update App styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar, { SidebarItem } from "./components/Sidebar.jsx";
-import Announcement from "./components/StudentDashboard.jsx";
+// import Announcement from "./components/StudentDashboard.jsx";
 import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx";
 import Enrollment from './components/Enrollment.jsx';
