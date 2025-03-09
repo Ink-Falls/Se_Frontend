ce50e4c7e2cbd11279746e046a2808de6d9abd90
stiipp <curilansteph456@gmail.com>
2025-03-09 22:01:18 +0800

    Create Error404 page

--- src/pages/Enrollment/Enrollment.jsx
+++ src/pages/Enrollment/Enrollment.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "/src/assets/images/ARALKADEMYLOGO.png";
+import logo from "../../assets/images/ARALKADEMYLOGO.png";
 
 function Enrollment() {
   const [email, setEmail] = useState("");
@@ -30,7 +30,10 @@ function Enrollment() {
         const errorData = await response.json();
         // Check for specific "not found" message from backend
 
-        if (response.status === 404 && errorData.message === 'Enrollment not found for this email') {
+        if (
+          response.status === 404 &&
+          errorData.message === "Enrollment not found for this email"
+        ) {
           setErrorMessage("Email not found"); // Set the specific error message
           setStatus("Unknown"); // Reset status
           setStatusColor("#F6BA18"); // Reset color
@@ -193,14 +196,14 @@ function Enrollment() {
                       {status.charAt(0).toUpperCase() + status.slice(1)}
                     </p>
                   </div>
-                                </div>  
-                            </form>
-                        </div>
-                    </div>
                 </div>
+              </form>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Enrollment;
