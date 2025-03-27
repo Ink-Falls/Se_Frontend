be526f89b19e7711e7df4bc281fca0439c4c930b
nctml02 <tristansigua02@gmail.com>
2025-03-27 22:51:04 +0800

    Add learner count to TeacherDashboard by fetching group members

--- src/pages/Teacher/TeacherDashboard.jsx
+++ src/pages/Teacher/TeacherDashboard.jsx
@@ -13,6 +13,7 @@ import Header from "../../components/common/layout/Header";
 import MobileNavBar from "../../components/common/layout/MobileNavbar";
 import { getTeacherCourses } from "../../services/courseService";
 import { useCourse } from "../../contexts/CourseContext";
+import { getGroupMembers } from "../../services/groupService";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 const Dashboard = () => {
@@ -58,6 +59,13 @@ const Dashboard = () => {
       setLoading(true);
       try {
         const coursesData = await getTeacherCourses();
+
+        // Get the number of learners in each course
+        for (const course of coursesData) {
+          const groupMembers = await getGroupMembers(course.learner_group_id); 
+          course.studentCount = groupMembers.length; 
+        }
+
         setCourses(coursesData);
         setError(null);
 
@@ -210,7 +218,7 @@ const Dashboard = () => {
                               d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                             />
                           </svg>
-                          {course.students?.length || 0} Learners
+                          {course.studentCount} {course.studentCount === 1 ? 'Learner' : 'Learners'}
                         </span>
                       </div>
                     </div>
